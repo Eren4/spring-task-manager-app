5f69d2d59175a3e160b0b1e5e5bdbdcecb16b8b0
Eren4 <erendenizkusu@gmail.com>
2023-08-29 17:05:39 +0300

    Added functionality about page navigation

--- taskmanager/src/main/java/com/netchum/taskmanager/controllers/TaskManagerController.java
+++ taskmanager/src/main/java/com/netchum/taskmanager/controllers/TaskManagerController.java
@@ -2,12 +2,23 @@ package com.netchum.taskmanager.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TaskManagerController {
 
     @GetMapping("/")
     public String getHomePage() {
+        return "home-page";
+    }
+
+    @PostMapping("/get-login")
+    public String getLoginScreen() {
+        return "login-page";
+    }
+
+    @PostMapping("/")
+    public String getMainScreen() {
         return "home-page";
     }
 }
