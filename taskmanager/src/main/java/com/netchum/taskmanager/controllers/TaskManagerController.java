842d04fc4395732f1894ce23d8a30533956da616
Eren4 <erendenizkusu@gmail.com>
2023-08-30 10:31:59 +0300

    Implemented the controller part

--- taskmanager/src/main/java/com/netchum/taskmanager/controllers/TaskManagerController.java
+++ taskmanager/src/main/java/com/netchum/taskmanager/controllers/TaskManagerController.java
@@ -1,11 +1,16 @@
 package com.netchum.taskmanager.controllers;
 
+import com.netchum.taskmanager.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TaskManagerController {
+
+    @Autowired
+    UserService userService;
 
     @GetMapping("/")
     public String getHomePage() {
@@ -25,5 +30,19 @@ public class TaskManagerController {
     @PostMapping("/")
     public String getMainScreen() {
         return "home-page";
+    }
+
+    @RequestMapping(value = "/login-page", method = RequestMethod.POST)
+    public String login(@RequestParam String email, @RequestParam String password,
+                        Model model) {
+        boolean isAuthenticated = userService.authenticateUser(email, password);
+
+        if(isAuthenticated) {
+            return "task-list";
+        }
+        else {
+            model.addAttribute("error", "Invalid email or password");
+            return "login";
+        }
     }
 }
