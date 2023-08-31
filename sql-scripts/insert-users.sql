bd00c43e8a5c78950348c1439bd876a57d6ef968
Eren4 <erendenizkusu@gmail.com>
2023-08-31 12:30:06 +0300

    Updated insert users

--- sql-scripts/insert-users.sql
+++ sql-scripts/insert-users.sql
@@ -3,6 +3,6 @@ DELETE FROM USERS;
 ALTER SEQUENCE USERS_USER_ID_SEQ RESTART WITH 1;
 
 INSERT INTO USERS (USERNAME, EMAIL, PASSWORD)
-VALUES ('eren', 'eren@email.com', 'eren123'),
-('jack', 'jack@email.com', 'jack123'),
-('zoe', 'zoe@email.com', 'zoe123');
+VALUES ('eren', 'eren@email.com', '$2a$10$hLv6pj/REtt0bF.2sHS0aeMiY5PBdoq4.lscqSMuaoN38m8O5u1cq'),
+('jack', 'jack@email.com', '$2a$10$1SD4hbRTlKarCOgY4Iqdk.srAcLy1O/mn.TAOQnUVKAusOz8HghL.'),
+('zoe', 'zoe@email.com', '$2a$10$CxXJFobb.VNFDWkgXbfA2OMwe4Ze4CItTGybOOVY18eya81VVwyMy');
